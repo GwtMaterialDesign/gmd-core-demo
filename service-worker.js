14ca86de3b9e8fd273756fc56a4831004be1f1cd
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2019-01-13 13:25:18 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,17 +1,124 @@
-var cacheName = 'dasdasdasdas';
+var cacheName = 'cache_1547355294771';
 
 var filesToCache = [
 
+    'GmdCoreDemo/clear.cache.gif',
+
+    'GmdCoreDemo/css/animation.css',
+
+    'GmdCoreDemo/css/animation.min.css',
+
+    'GmdCoreDemo/css/fontawesome-all.css',
+
+    'GmdCoreDemo/css/fontawesome-all.min.css',
+
+    'GmdCoreDemo/css/material-icons.css',
+
+    'GmdCoreDemo/css/material-icons.min.css',
+
+    'GmdCoreDemo/css/materialize.blue.css',
+
+    'GmdCoreDemo/css/materialize.blue.min.css',
+
+    'GmdCoreDemo/css/materialize.css',
+
+    'GmdCoreDemo/css/materialize.min.css',
+
+    'GmdCoreDemo/font/material-icons/MaterialIcons-Regular.eot',
+
+    'GmdCoreDemo/font/material-icons/MaterialIcons-Regular.ttf',
+
+    'GmdCoreDemo/font/material-icons/MaterialIcons-Regular.woff',
+
+    'GmdCoreDemo/font/material-icons/MaterialIcons-Regular.woff2',
+
+    'GmdCoreDemo/font/roboto/Roboto-Bold.ttf',
+
+    'GmdCoreDemo/font/roboto/Roboto-Bold.woff',
+
+    'GmdCoreDemo/font/roboto/Roboto-Bold.woff2',
+
+    'GmdCoreDemo/font/roboto/Roboto-Light.ttf',
+
+    'GmdCoreDemo/font/roboto/Roboto-Light.woff',
+
+    'GmdCoreDemo/font/roboto/Roboto-Light.woff2',
+
+    'GmdCoreDemo/font/roboto/Roboto-Medium.ttf',
+
+    'GmdCoreDemo/font/roboto/Roboto-Medium.woff',
+
+    'GmdCoreDemo/font/roboto/Roboto-Medium.woff2',
+
+    'GmdCoreDemo/font/roboto/Roboto-Regular.ttf',
+
+    'GmdCoreDemo/font/roboto/Roboto-Regular.woff',
+
+    'GmdCoreDemo/font/roboto/Roboto-Regular.woff2',
+
+    'GmdCoreDemo/font/roboto/Roboto-Thin.ttf',
+
+    'GmdCoreDemo/font/roboto/Roboto-Thin.woff',
+
+    'GmdCoreDemo/font/roboto/Roboto-Thin.woff2',
+
+    'GmdCoreDemo/fontawesome-icons/fa-brands-400.eot',
+
+    'GmdCoreDemo/fontawesome-icons/fa-brands-400.ttf',
+
+    'GmdCoreDemo/fontawesome-icons/fa-brands-400.woff',
+
+    'GmdCoreDemo/fontawesome-icons/fa-brands-400.woff2',
+
+    'GmdCoreDemo/fontawesome-icons/fa-regular-400.eot',
+
+    'GmdCoreDemo/fontawesome-icons/fa-regular-400.ttf',
+
+    'GmdCoreDemo/fontawesome-icons/fa-regular-400.woff',
+
+    'GmdCoreDemo/fontawesome-icons/fa-regular-400.woff2',
+
+    'GmdCoreDemo/fontawesome-icons/fa-solid-900.eot',
+
+    'GmdCoreDemo/fontawesome-icons/fa-solid-900.ttf',
+
+    'GmdCoreDemo/fontawesome-icons/fa-solid-900.woff',
+
+    'GmdCoreDemo/fontawesome-icons/fa-solid-900.woff2',
+
+    'GmdCoreDemo/GmdCoreDemo.nocache.js',
+
+    'index.html',
+
+    'launcher-icons/launcher1x.png',
+
+    'launcher-icons/launcher2x.png',
+
+    'launcher-icons/launcher4x.png',
+
+    'launcher-icons/launcher5x.png',
+
+    'splash/font/Roboto-Regular.eot',
+
+    'splash/font/Roboto-Regular.ttf',
+
+    'splash/font/Roboto-Regular.woff',
+
+    'splash/font/Roboto-Regular.woff2',
+
+    'splash/splash.css',
+
+    'splash/splash.js'
 ];
 
 /**
  * The install event is your chance to cache everything you need before being able to control clients. The promise you
  * pass to event.waitUntil() lets the browser know when your install completes, and if it was successful.
  */
-self.addEventListener('install', function (e) {
+self.addEventListener('install', function(e) {
     console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.open(cacheName).then(function (cache) {
+        caches.open(cacheName).then(function(cache) {
             console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(filesToCache);
         })
@@ -22,16 +129,12 @@ self.addEventListener('install', function (e) {
  * Once your service worker is ready to control clients and handle functional events like push and sync, you'll get an
  * activate event. But that doesn't mean the page that called .register() will be controlled.
  */
-self.addEventListener('activate', function (e) {
-    console.log('[ServiceWorker] Activate');
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
     e.waitUntil(
-        caches.keys().then(function (keyList) {
-            return Promise.all(keyList.map(function (key) {
-                console.log('[ServiceWorker] Removing old cache', key);
-                if (key !== cacheName) {
-                    return caches.delete(key);
-                }
-            }));
+        caches.open(cacheName).then(function(cache) {
+            console.log('[ServiceWorker] Caching app shell');
+            return cache.addAll(filesToCache);
         })
     );
 });
@@ -40,10 +143,10 @@ self.addEventListener('activate', function (e) {
  * It contains information about the fetch, including the request and how the receiver will treat the response.
  * It provides the event.respondWith() method, which allows us to provide a response to this fetch.
  */
-self.addEventListener('fetch', function (e) {
+self.addEventListener('fetch', function(e) {
     console.log('[ServiceWorker] Fetch', e.request.url);
     e.respondWith(
-        caches.match(e.request).then(function (response) {
+        caches.match(e.request).then(function(response) {
             return response || fetch(e.request);
         })
     );
@@ -54,7 +157,6 @@ self.addEventListener('fetch', function (e) {
  * This will also provide you a Notification UI build from the JSON Payload provided before sending the message.
  */
 self.addEventListener('push', function (event) {
-    console.log("Service Worker Push Received");
     var json = event.data.json();
     // The Notification Data which contains all required parameters to display a notification ui.
     var data = JSON.parse(json.data.model);
