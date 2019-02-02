ae86bea3160b026ecff30adcfd8184417d0ca725
travis-ci <travis@travis-ci.org>
2019-02-02 07:01:52 +0000

    Auto-push demo to gh-pages successful. (Travis build: 29)

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,6 @@
-var cacheName = 'cache_15473552947712';
+var cacheName = 'cache_1547355294771';
 
 var filesToCache = [
-    
-    'GmdCoreDemo/60CC6903DD35737DDFAA06FDADC31250.cache.js',
 
     'GmdCoreDemo/clear.cache.gif',
 
@@ -145,29 +143,12 @@ self.addEventListener('install', function(e) {
  * It contains information about the fetch, including the request and how the receiver will treat the response.
  * It provides the event.respondWith() method, which allows us to provide a response to this fetch.
  */
-addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request)
-            .then(function(response) {
-                if (response) {
-                    return response;     // if valid response is found in cache return it
-                } else {
-                    return fetch(event.request)     //fetch from internet
-                        .then(function(res) {
-                            return caches.open(cacheName)
-                                .then(function(cache) {
-                                    cache.put(event.request.url, res.clone());    //save the response for future
-                                    return res;   // return the fetched data
-                                })
-                        })
-                        .catch(function(err) {       // fallback mechanism
-                            /*return caches.open(CACHE_CONTAINING_ERROR_MESSAGES)
-                                .then(function(cache) {
-                                    return cache.match('/offline.html');
-                                });*/
-                        });
-                }
-            })
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function(response) {
+            return response || fetch(e.request);
+        })
     );
 });
 
