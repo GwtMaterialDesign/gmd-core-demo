f3a88bf7727c7b0d97b9b8e8b9b4878710020677
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-01-08 06:14:14 +0800

    Updated version to 2.3

--- src/main/java/gmd/core/demo/client/constants/AppConstants.java
+++ src/main/java/gmd/core/demo/client/constants/AppConstants.java
@@ -4,7 +4,7 @@ public interface AppConstants {
 
     String NAME = "gwt-material";
     int YEAR = 2019;
-    double VERSION = 2.2;
+    double VERSION = 2.3;
     String GITHUB_SOURCE_CODE = "https://github.com/GwtMaterialDesign/gmd-core-demo/tree/master/src/main/java/";
     String GITHUB_REPO = "https://github.com/GwtMaterialDesign/gwt-material";
     String GITTER_CHANNEL = "https://gitter.im/GwtMaterialDesign/gwt-material";
