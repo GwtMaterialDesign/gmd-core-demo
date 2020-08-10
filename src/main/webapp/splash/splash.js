8db233e05c1d94e3c490412e5ceb9fbd725bf30b
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2020-08-10 17:22:47 +0800

    Preparation for 2.4.0-rc2 release.

--- src/main/webapp/splash/splash.js
+++ src/main/webapp/splash/splash.js
@@ -12,7 +12,7 @@ title.innerHTML = "gwt-material-design";
 
 var description = document.createElement("span");
 description.className = "description";
-description.innerHTML = "version 2.4.0-rc1";
+description.innerHTML = "version 2.4.0";
 
 var progress = document.createElement("div");
 progress.className = "progress";
