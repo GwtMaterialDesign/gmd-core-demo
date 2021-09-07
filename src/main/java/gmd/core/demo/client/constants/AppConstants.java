7f0e203b4094ebfb21c36e95b2a0207035ba2cbc
Mark Kevin Baldemor <kevzlou7979@gmail.com>
2021-09-07 15:12:21 +0800

    Transitioning to 2.6.0-SNAPSHOT

--- src/main/java/gmd/core/demo/client/constants/AppConstants.java
+++ src/main/java/gmd/core/demo/client/constants/AppConstants.java
@@ -3,7 +3,7 @@ package gmd.core.demo.client.constants;
 public interface AppConstants {
 
     String NAME = "gwt-material";
-    String VERSION = "2.5.0";
+    String VERSION = "2.6.0";
     String GITHUB_SOURCE_CODE = "https://github.com/GwtMaterialDesign/gmd-core-demo/tree/master/src/main/java/";
     String GITHUB_REPO = "https://github.com/GwtMaterialDesign/gwt-material";
     String SLACK_CHANNEL = "https://gmd-project.slack.com";
