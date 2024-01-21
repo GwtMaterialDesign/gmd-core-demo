de4ec73c0a147222e1e8c4f4311f234b0b149f93
Hansjörg Hofer <hansjoerghofer723@gmail.com>
2024-01-21 15:10:17 +0100

    Iteration vs Collection.addAll()

--- src/main/java/gmd/core/demo/client/application/page/search/SearchView.java
+++ src/main/java/gmd/core/demo/client/application/page/search/SearchView.java
@@ -7,9 +7,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -72,9 +72,9 @@ public class SearchView extends ViewImpl implements SearchPresenter.MyView {
 
         // Populate the search keyword into search component
         List<SearchObject> objects = new ArrayList<>();
-        for(Hero hero : DataHelper.getAllHeroes()){
-            objects.add(hero);
-        }
+
+        objects.addAll(DataHelper.getAllHeroes());
+
         txtSearch.setListSearches(objects);
 
         txtSearch.setSelectedObject(objects.get(3));
